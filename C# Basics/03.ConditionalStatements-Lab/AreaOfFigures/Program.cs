b49066c96467454e49f5f1404c3cf8a237e9ced4
LyuboSokolov <lyubomir.sokolov@gmail.com>
2022-01-10 22:44:47 +0200

    Add exercise

---
+++ C# Basics/03.ConditionalStatements-Lab/AreaOfFigures/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string shape = Console.ReadLine();
+            double area = 0;
+
+            if (shape == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                area = side * side;
+
+            }
+            else if (shape == "rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                area = sideA * sideB;
+            }
+            else if (shape == "circle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                area = Math.PI * side * side;
+
+            }
+            else if (shape == "triangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                area = sideA * sideB / 2;
+
+            }
+            Console.WriteLine(Math.Round(area, 3));
+        }
+    }
+}
---
+++ C# Basics/03.ConditionalStatements-Lab/ToyShop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ToyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceTrip = double.Parse(Console.ReadLine());
+            int puzzle = int.Parse(Console.ReadLine());
+            int kukli = int.Parse(Console.ReadLine());
+            int beard = int.Parse(Console.ReadLine());
+            int minion = int.Parse(Console.ReadLine());
+            int truck = int.Parse(Console.ReadLine());
+            double sum = 0;
+            int pcsSum = 0;
+
+            sum = puzzle * 2.60 + kukli * 3 + beard * 4.10 + minion * 8.20 + truck * 2;
+            pcsSum = puzzle + kukli + beard + minion + truck;
+
+            if (pcsSum >= 50)
+            {
+                sum = sum * 0.75;
+            }
+
+            sum = sum * 0.9;
+            if (priceTrip <= sum)
+            {
+                Console.WriteLine($"Yes! {sum - priceTrip:f2} lv left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! {priceTrip - sum:f2} lv needed.");
+            }
+        }
+    }
+}
