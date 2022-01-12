695ba418eeb624caeeff11d221374e6ef2fca4df
LyuboSokolov <lyubomir.sokolov@gmail.com>
2022-01-12 22:48:10 +0200

    Add exercise

---
+++ C# Basics/05.Loops-Part1-Lab/Graduation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double grades = 1;
+            double sum = 0;
+
+            while (grades <= 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+
+                if (grade >= 4)
+                {
+                    sum += grade;
+                    ++grades;
+                }
+            }
+            double average = sum / 12;
+            Console.WriteLine($"{name} graduated. Average grade: {average:f2}");
+        }
+    }
+}
---
+++ C# Basics/05.Loops-Part1-Lab/GraduationPt.2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GraduationPt._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double grades = 1;
+            double sum = 0;
+            int count = 0;
+
+            while (grades <= 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+
+                if (grade >= 4)
+                {
+                    sum += grade;
+                    ++grades;
+                }
+                else
+                {
+                    ++count;
+                    if (count > 1)
+                    {
+                        break;
+                    }
+                }
+            }
+            if (grades == 13)
+            {
+                double average = sum / 12;
+                Console.WriteLine($"{name} graduated. Average grade: {average:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"{name} has been excluded at {grades} grade");
+            }
+        }
+    }
+}
