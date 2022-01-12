6eccd7a8023ff3ff9c6d1d3a1a40f94241d1464e
LyuboSokolov <lyubomir.sokolov@gmail.com>
2022-01-12 22:50:04 +0200

    Add more exercise

---
+++ C# Basics/05.Loops-Part1-Lab/Moving/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+            string input = String.Empty;
+            int box = 0;
+            int allBox = 0;
+            int freeSpace = 0;
+
+            freeSpace = w * l * h;
+
+            while (input != "Done")
+            {
+                input = Console.ReadLine();
+                if (input == "Done")
+                {
+                    Console.WriteLine($"{freeSpace - allBox} Cubic meters left.");
+                    break;
+
+                }
+                box = int.Parse(input);
+                allBox += box;
+                if (allBox > freeSpace)
+                {
+                    Console.WriteLine($"No more free space! You need {allBox - freeSpace} Cubic meters more.");
+                    break;
+                }
+            }
+        }
+    }
+}
