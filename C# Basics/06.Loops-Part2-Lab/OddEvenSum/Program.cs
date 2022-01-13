4c047bdb929f78de97523c86ffa7cd059d9bca26
LyuboSokolov <lyubomir.sokolov@gmail.com>
2022-01-13 09:53:17 +0200

    Add exercise

---
+++ C# Basics/06.Loops-Part2-Lab/OddEvenSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OddEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int even = 0;
+            int odd = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    even += num;
+                }
+                else
+                {
+                    odd += num;
+                }
+            }
+            if (odd == even)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {even}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(even - odd)}");
+            }
+        }
+    }
+}
