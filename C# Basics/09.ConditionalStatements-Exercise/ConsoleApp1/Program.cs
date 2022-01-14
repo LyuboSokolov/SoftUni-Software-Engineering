8ac9ed69950be0fb5ae90bae2b3927c919637955
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-01-15 00:35:04 +0200

    add exercise

---
+++ C# Basics/09.ConditionalStatements-Exercise/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            
+            int sumMinutes = 0;
+            
+
+            sumMinutes = minutes + 15;
+            
+
+            if (sumMinutes>=60)
+            {
+                hour = hour + 1;
+                sumMinutes = sumMinutes % 60;
+            }
+          
+            if (hour>=24)
+            {
+                hour = 0;
+            }
+            
+
+            if (sumMinutes<10)
+            {
+                Console.WriteLine($"{hour}:0{sumMinutes}");
+            }
+            else
+            {
+                Console.WriteLine($"{hour}:{sumMinutes}");
+            }
+
+        }
+    }
+}
