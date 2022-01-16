9d6358d0bf0f54fa45ef107e92cdbc3cea56d472
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-01-16 22:56:57 +0200

    Add exercise

---
+++ C# Basics/09.NestedLoops-Lab/SumOfTwoNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingNumber = int.Parse(Console.ReadLine());
+            int finishNumber = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int combinations = 0;
+            bool isFound = false;
+
+            for (int i = startingNumber; i <= finishNumber; i++)
+            {
+                for (int m = startingNumber; m <= finishNumber; m++)
+                {
+                    combinations++;
+                    if (i + m == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{combinations} ({i} + {m} = {magicNumber})");
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (isFound)
+                {
+                    break;
+                }
+            }
+            if (!isFound)
+            {
+                Console.WriteLine($"{combinations} combinations - neither equals {magicNumber}");
+            }
+        }
+    }
+}
