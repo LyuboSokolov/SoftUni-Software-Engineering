f173dc8222c1ac68ae8c76003f9693a88ca131fc
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-01-22 19:17:39 +0200

    add more exercise

---
+++ C#Fundamentals/01.BasicSyntaxConditionalStatementsAndLoopsLab/MultiplicationTable2.0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MultiplicationTable2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            int i = number2;
+            do
+            {
+                Console.WriteLine($"{number} X {i} = {number * i}");
+                i++;
+
+            } while (i <= 10);
+        }
+    }
+}
