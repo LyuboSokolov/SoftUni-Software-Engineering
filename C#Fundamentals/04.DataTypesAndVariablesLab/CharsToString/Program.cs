cf23ea8b78f721d0ca0d405e492e4e6cb5d0df64
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-01-26 12:34:30 +0200

    exercise

---
+++ C#Fundamentals/04.DataTypesAndVariablesLab/CharsToString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CharsToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstSymbol = char.Parse(Console.ReadLine());
+            char secondSymbol = char.Parse(Console.ReadLine());
+            char thirdSymbol = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstSymbol}{secondSymbol}{thirdSymbol}");
+        }
+    }
+}
---
+++ C#Fundamentals/04.DataTypesAndVariablesLab/LowerOrUpper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LowerOrUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstLetter = char.Parse(Console.ReadLine());
+
+            if ((int)firstLetter >= 65 && (int)firstLetter <= 90)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
