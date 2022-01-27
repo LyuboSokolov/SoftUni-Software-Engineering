9cd5d7338a9838a70a1ef2fbf3f7cdf2ee2bd38c
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-01-27 22:37:06 +0200

    add exercise

---
+++ C#Fundamentals/04.DataTypesAndVariablesLab/PoundsToDollars/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PoundsToDollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pounds = double.Parse(Console.ReadLine());
+            decimal dolars = 0;
+
+            dolars = (decimal)pounds * 1.31M;
+
+            Console.WriteLine($"{dolars:f3}");
+        }
+    }
+}
