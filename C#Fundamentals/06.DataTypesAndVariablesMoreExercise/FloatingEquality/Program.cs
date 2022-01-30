f34e35666e7e6ed85a81795717d94f350507addf
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-01-30 22:10:47 +0200

    more exercise

---
+++ C#Fundamentals/06.DataTypesAndVariablesMoreExercise/FloatingEquality/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FloatingEquality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberA = double.Parse(Console.ReadLine());
+            double numberB = double.Parse(Console.ReadLine());
+
+            double precision = Math.Abs(numberA - numberB);
+
+            if (precision < 0.000001)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+    }
+}
