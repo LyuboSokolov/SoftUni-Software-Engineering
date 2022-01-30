ba9cdf705e0d4b21dfce3b57021cd522fe3d0d82
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-01-30 22:13:35 +0200

    add exercise

---
+++ C#Fundamentals/06.DataTypesAndVariablesMoreExercise/RefactoringPrimeChecker/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RefactoringPrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= n; i++)
+            {
+                bool isSimple = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isSimple = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{i} -> {isSimple.ToString().ToLower()}");
+            }
+        }
+    }
+}
