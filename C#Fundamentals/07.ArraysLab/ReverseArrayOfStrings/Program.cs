eae3e6d6ad6ade2fb7ed8daf872ff30e03fa5711
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-01 02:35:08 +0200

    add exercise

---
+++ C#Fundamentals/07.ArraysLab/ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+            Console.WriteLine(string.Join(" ", input.Reverse()));
+        }
+    }
+}
