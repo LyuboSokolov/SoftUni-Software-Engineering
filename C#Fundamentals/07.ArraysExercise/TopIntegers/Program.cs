8d322452515659d12463ec9bcd48c26487fb1f35
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-03 21:32:33 +0200

    add exercise

---
+++ C#Fundamentals/07.ArraysExercise/TopIntegers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace TopIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+            int V = numbers.Length;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i + 1 < numbers.Length)
+                {
+                    if (numbers[i] > numbers[i + 1])
+                    {
+                        Console.Write($"{numbers[i]} ");
+                    }
+                }
+                else
+                {
+                    Console.Write($"{numbers[numbers.Length - 1]}");
+                }
+            }
+        }
+    }
+}
