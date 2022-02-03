b842e3e2480980967a60cf3c60e9da44f08496c6
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-03 21:30:18 +0200

    add exercise

---
+++ C#Fundamentals/07.ArraysExercise/EqualSum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace EqualSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .ToArray();
+            int leftSum = 0;
+            int rightSum = 0;
+            bool isEqual = false;
+
+            for (int curr = 0; curr < arr.Length; curr++)
+            {
+                rightSum = 0;
+                for (int i = curr + 1; i < arr.Length; i++)
+                {
+                    rightSum += arr[i];
+                }
+
+                leftSum = 0;
+                for (int j = curr - 1; j >= 0; j--)
+                {
+                    leftSum += arr[j];
+                }
+                if (rightSum == leftSum)
+                {
+                    Console.WriteLine(curr);
+                    isEqual = true;
+                    break;
+                }
+            }
+            if (!isEqual && arr.Length > 1)
+            {
+                Console.WriteLine("no");
+            }
+            if (rightSum != leftSum && arr.Length == 1)
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
+
---
+++ C#Fundamentals/07.ArraysExercise/Train/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] peopleInWagon = new int[n];
+            int allPeople = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                peopleInWagon[i] = int.Parse(Console.ReadLine());
+                allPeople += peopleInWagon[i];
+            }
+
+            Console.WriteLine(string.Join(" ", peopleInWagon));
+            Console.WriteLine(allPeople);
+        }
+    }
+}
