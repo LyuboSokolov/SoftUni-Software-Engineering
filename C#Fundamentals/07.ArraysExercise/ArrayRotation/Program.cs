ff8236b1b46429cdba1573641334d3e0deb320ba
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-03 21:28:44 +0200

    add exercise

---
+++ C#Fundamentals/07.ArraysExercise/ArrayRotation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int rotation = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rotation; i++)
+            {
+                int firstnumber = numbers[0];
+
+                for (int j = 1; j < numbers.Length; j++)
+                {
+                    int currentNumber = numbers[j];
+                    numbers[j - 1] = currentNumber;
+                }
+                numbers[numbers.Length - 1] = firstnumber;
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
---
+++ C#Fundamentals/07.ArraysExercise/MagicSum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                                .Split()
+                                .Select(int.Parse)
+                                .ToArray();
+            int givenNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int currentNumber = arr[i];
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (currentNumber + arr[j] == givenNumber)
+                    {
+                        Console.WriteLine($"{currentNumber} {arr[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
