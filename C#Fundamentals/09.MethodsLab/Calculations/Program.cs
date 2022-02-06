bc45f5507370ddcd67895686de20a2d733d331c5
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-06 19:56:29 +0200

    add exercise

---
+++ C#Fundamentals/09.MethodsLab/Calculations/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string operation = Console.ReadLine();
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            if (operation == "add")
+            {
+                ADDNumbers(firstNumber, secondNumber);
+            }
+            else if (operation == "multiply")
+            {
+                MultiplyNumbers(firstNumber, secondNumber);
+            }
+            else if (operation == "subtract")
+            {
+                SubtracktNumbers(firstNumber, secondNumber);
+            }
+            else if (operation == "divide")
+            {
+                DivideNumbers(firstNumber, secondNumber);
+            }
+        }
+
+        static void DivideNumbers(int firstNumber, int secondNumber)
+        {
+            if (secondNumber != 0)
+            {
+                Console.WriteLine(firstNumber / secondNumber);
+            }
+
+        }
+        static void SubtracktNumbers(int firstNumber, int secondNumber)
+        {
+            Console.WriteLine(firstNumber - secondNumber);
+        }
+
+        static void MultiplyNumbers(int firstNumber, int secondNumber)
+        {
+            Console.WriteLine(firstNumber * secondNumber);
+        }
+
+        static void ADDNumbers(int firstNumber, int secondNumber)
+        {
+            Console.WriteLine(firstNumber + secondNumber);
+        }
+    }
+}
+
---
+++ C#Fundamentals/09.MethodsLab/PrintingTriangle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                PrintingTriangle(1, i);
+            }
+            for (int i = n - 1; i >= 1; i--)
+            {
+                PrintingTriangle(1, i);
+            }
+        }
+
+        static void PrintingTriangle(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
