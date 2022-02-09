3b4541e1d3387502b9bb28f79cd508138a0e7c4d
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-09 23:17:52 +0200

    add exercise

---
+++ C#Fundamentals/10.MethodsExercise/AddAndSubtract/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AddAndSubtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int sum = GetSum(firstNumber, secondNumber);
+            Console.WriteLine(GetSubtract(sum, thirdNumber)); ;
+        }
+
+        static int GetSubtract(int sum, int thirdNumber)
+        {
+            return sum - thirdNumber;
+        }
+
+        static int GetSum(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
+
---
+++ C#Fundamentals/10.MethodsExercise/FactorialDivision/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace FactorialDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            double firstNumberFactorial = GetFirstNumberFactorial(firstNumber);
+            double secondNumberFactorial = GetSecondNumberFactorial(secondNumber);
+
+            FactorialDivision(firstNumberFactorial, secondNumberFactorial);
+
+        }
+
+        static void FactorialDivision(double firstNumberFactorial, double secondNumberFactorial)
+        {
+            double division = 0;
+
+            division = firstNumberFactorial / secondNumberFactorial;
+
+            Console.WriteLine($"{division:f2}");
+        }
+
+        static double GetSecondNumberFactorial(double secondNumber)
+        {
+            double factorial = 1;
+            for (int i = 1; i <= secondNumber; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+
+        static double GetFirstNumberFactorial(double firstNumber)
+        {
+            double factorial = 1;
+            for (int i = 1; i <= firstNumber; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+}
