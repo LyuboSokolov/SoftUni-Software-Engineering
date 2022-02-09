7b6040fcc24f8d8032b09ec8df41455650731af2
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-09 23:20:45 +0200

    add exercise

---
+++ C#Fundamentals/10.MethodsExercise/MiddleCharacters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MiddleCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            PrintMiddleChar(input);
+        }
+        static void PrintMiddleChar(string input)
+        {
+            string middleSymbols = string.Empty;
+            if (input.Length % 2 == 0)
+            {
+                char firstMiddleSymbol = input[(input.Length - 1) / 2];
+                char secondMiddleSymbol = input[input.Length / 2];
+                middleSymbols += firstMiddleSymbol;
+                middleSymbols += secondMiddleSymbol;
+            }
+            else
+            {
+                middleSymbols = input[(input.Length / 2)].ToString();
+            }
+            Console.WriteLine(middleSymbols);
+        }
+    }
+}
---
+++ C#Fundamentals/10.MethodsExercise/TopNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TopNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            GetDivideDegitByEight(n);
+        }
+
+        static void GetDivideDegitByEight(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                int sum = 0;
+                bool isValid = false;
+                int currentNumber = i;
+                while (currentNumber > 0)
+                {
+                    int currendDigit = currentNumber % 10;
+                    sum += currendDigit;
+                    if (currendDigit % 2 != 0)
+                    {
+                        isValid = true;
+                    }
+                    currentNumber /= 10;
+                }
+                if (isValid)
+                {
+                    if (sum % 8 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
