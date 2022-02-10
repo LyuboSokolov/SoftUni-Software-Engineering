f6381797375596a30875f2013d6c1a588e290ab0
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-10 20:24:36 +0200

    add exercise

---
+++ C#Fundamentals/10.MethodsExercise/PalindromeIntegers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PalindromeIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string reverseInput = string.Empty;
+                for (int i = input.Length - 1; i >= 0; i--)
+                {
+                    reverseInput += input[i];
+                }
+                if (input == reverseInput)
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+    }
+}
