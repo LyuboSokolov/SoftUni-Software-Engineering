b4cba0899821a4ac6c9c21e081f94bab668b71d4
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-15 23:54:38 +0200

    add exercise

---
+++ C#Fundamentals/12.ListsExercise/BombNumbers/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BombNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbersSequence = Console.ReadLine()
+               .Split()
+               .Select(int.Parse)
+               .ToList();
+
+            int[] bombNumberAndPower = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int bombNumber = bombNumberAndPower[0];
+            int powerNumber = bombNumberAndPower[1];
+
+            for (int i = 0; i < numbersSequence.Count; i++)
+            {
+
+
+                if (bombNumber == numbersSequence[i])
+                {
+                    int startIndex = i - powerNumber;
+                    int endIndex = i + powerNumber;
+
+                    if (startIndex < 0)
+                    {
+                        startIndex = 0;
+                    }
+                    if (endIndex > numbersSequence.Count - 1)
+                    {
+                        endIndex = numbersSequence.Count - 1;
+                    }
+
+                    numbersSequence.RemoveRange(startIndex, endIndex - startIndex + 1);
+                    i = startIndex - 1;
+                }
+
+
+
+            }
+
+            Console.WriteLine(numbersSequence.Sum());
+        }
+    }
+}
---
+++ C#Fundamentals/12.ListsExercise/ListOperations/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string[] commands = Console.ReadLine()
+                .ToUpper()
+                .Split()
+                .ToArray();
+
+            while (commands[0] != "END")
+            {
+                switch (commands[0])
+                {
+                    case "ADD":
+                        numbers.Add(int.Parse(commands[1]));
+                        break;
+                    case "INSERT":
+
+                        if (isValidIndex(int.Parse(commands[2]), numbers.Count))
+                        {
+                            Console.WriteLine("Invalid index");
+                        }
+                        else
+                        {
+                            numbers.Insert(int.Parse(commands[2]), int.Parse(commands[1]));
+                        }
+
+                        break;
+                    case "REMOVE":
+                        if (isValidIndex(int.Parse(commands[1]), numbers.Count))
+                        {
+                            Console.WriteLine("Invalid index");
+                        }
+                        else
+                        {
+                            numbers.RemoveAt(int.Parse(commands[1]));
+                        }
+
+                        break;
+
+                    case "SHIFT":
+                        if (commands[1] == "LEFT")
+                        {
+                            for (int i = 0; i < int.Parse(commands[2]); i++)
+                            {
+                                int firstNumber = numbers[0];
+                                numbers.Remove(firstNumber);
+                                numbers.Add(firstNumber);
+                            }
+                        }
+                        else
+                        {
+                            for (int i = 0; i < int.Parse(commands[2]); i++)
+                            {
+                                int lastNumber = numbers[numbers.Count - 1];
+                                numbers.Remove(lastNumber);
+                                numbers.Insert(0, lastNumber);
+                            }
+                        }
+
+                        break;
+                    default:
+                        break;
+                }
+                commands = Console.ReadLine()
+                .ToUpper()
+                .Split()
+                .ToArray();
+
+            }
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+
+        static bool isValidIndex(int index, int count)
+        {
+            return index >= count || index < 0;
+        }
+    }
+    }
+
