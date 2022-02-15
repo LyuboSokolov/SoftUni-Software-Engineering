b9801f59ca32e40a12d1ca3d62ad8f805b78d51f
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-15 23:56:49 +0200

    add exercise

---
+++ C#Fundamentals/12.ListsExercise/AppendArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine()
+                .Split('|')
+                .ToList();
+
+            numbers.Reverse();
+            List<string> results = new List<string>();
+
+            foreach (string number in numbers)
+            {
+
+                string[] currentNumber = number
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                foreach (string nuberToAdd in currentNumber)
+                {
+                    results.Add(nuberToAdd);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', results));
+        }
+    }
+}
