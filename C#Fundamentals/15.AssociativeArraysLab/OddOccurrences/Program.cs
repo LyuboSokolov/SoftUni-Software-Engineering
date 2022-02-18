9912ffcf09a37030e389ed1e680666e52e04867f
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-18 21:51:22 +0200

    add exercise

---
+++ C#Fundamentals/15.AssociativeArraysLab/OddOccurrences/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+               .ToLower()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> oddWords = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (oddWords.ContainsKey(word))
+                {
+                    oddWords[word]++;
+                }
+                else
+                {
+                    oddWords.Add(word, 1);
+                }
+            }
+
+            foreach (var word in oddWords)
+            {
+                if (word.Value % 2 == 1)
+                {
+                    Console.Write($"{word.Key} ");
+                }
+            }
+        }
+    }
+}
