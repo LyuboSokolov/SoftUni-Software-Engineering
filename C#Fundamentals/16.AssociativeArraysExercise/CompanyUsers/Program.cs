22f3ef0adf110ee5cebb4655eccf8c31270e6bf2
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-20 11:06:22 +0200

    add exercise

---
+++ C#Fundamentals/16.AssociativeArraysExercise/CompanyUsers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CompanyUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+               .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, List<string>> companys = new Dictionary<string, List<string>>();
+
+            while (input[0] != "End")
+            {
+                string companyName = input[0];
+                string employeeId = input[1];
+
+                if (companys.ContainsKey(companyName) == false)
+                {
+                    companys.Add(companyName, new List<string>());
+                }
+                if (companys[companyName].Contains(employeeId))
+                {
+                    input = Console.ReadLine()
+                .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                    continue;
+                }
+                companys[companyName].Add(employeeId);
+
+                input = Console.ReadLine()
+                .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            companys = companys.OrderBy(n => n.Key).ToDictionary(a => a.Key, a => a.Value);
+
+            foreach (var company in companys)
+            {
+                Console.WriteLine(company.Key);
+                foreach (var item in company.Value)
+                {
+                    Console.WriteLine($"-- {item}");
+                }
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/16.AssociativeArraysExercise/StudentAcademy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string studentName = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+
+                if (students.ContainsKey(studentName) == false)
+                {
+                    students.Add(studentName, new List<double>());
+                }
+
+                students[studentName].Add(grade);
+            }
+
+            foreach (var student in students.OrderByDescending(n => n.Value.Average()))
+            {
+                if (student.Value.Average() >= 4.50)
+                {
+                    Console.WriteLine($"{student.Key} -> { student.Value.Average():f2}");
+                }
+
+            }
+        }
+    }
+}
