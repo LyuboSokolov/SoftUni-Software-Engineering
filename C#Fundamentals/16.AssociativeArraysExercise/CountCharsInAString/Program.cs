d7f493219836b3b00b05848225362a40baaaeaca
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-21 22:08:22 +0200

    add exercise

---
+++ C#Fundamentals/16.AssociativeArraysExercise/CountCharsInAString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountCharsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<char, int> characters = new Dictionary<char, int>();
+
+            foreach (var word in words)
+            {
+                foreach (char letter in word)
+                {
+                    if (characters.ContainsKey(letter))
+                    {
+                        characters[letter]++;
+                    }
+                    else
+                    {
+                        characters.Add(letter, 1);
+                    }
+                }
+            }
+
+            foreach (var chars in characters)
+            {
+                Console.WriteLine($"{chars.Key} -> {chars.Value}");
+            }
+        }
+    }
+}
