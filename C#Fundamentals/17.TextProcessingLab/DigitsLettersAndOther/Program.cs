f4e863deb63d0a14fa8954f1fa0ed707661748c3
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-22 20:58:27 +0200

    add exercise

---
+++ C#Fundamentals/17.TextProcessingLab/DigitsLettersAndOther/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DigitsLettersAndOther
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string letter = string.Empty;
+            string digits = string.Empty;
+            string symbols = string.Empty;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currentSymbol = text[i];
+                if ((currentSymbol >= 65 && currentSymbol <= 90) || (currentSymbol >= 97 && currentSymbol <= 122))
+                {
+                    letter += currentSymbol;
+                }
+                else if (currentSymbol >= 48 && currentSymbol <= 57)
+                {
+                    digits += currentSymbol;
+                }
+                else
+                {
+                    symbols += currentSymbol;
+                }
+            }
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letter);
+            Console.WriteLine(symbols);
+        }
+    }
+}
---
+++ C#Fundamentals/17.TextProcessingLab/ReverseStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string reversingInput = new string(input.Reverse().ToArray());
+
+                Console.WriteLine($"{input} = {reversingInput}");
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
