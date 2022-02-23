b0972f12ec85cfd50de5658091f0d66dcdc0b139
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-23 22:03:31 +0200

    add exercise

---
+++ C#Fundamentals/17.TextProcessingLab/Substring/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string removeWord = Console.ReadLine().ToLower();
+            string text = Console.ReadLine();
+
+            while (text.Contains(removeWord) != false)
+            {
+                int index = text.IndexOf(removeWord);
+                text = text.Remove(index, removeWord.Length);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ C#Fundamentals/17.TextProcessingLab/TextFilter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWord = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string text = Console.ReadLine();
+            string replaceWord = string.Empty;
+
+            foreach (var word in bannedWord)
+            {
+                replaceWord = new string('*', word.Length);
+                text = text.Replace(word, replaceWord);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
