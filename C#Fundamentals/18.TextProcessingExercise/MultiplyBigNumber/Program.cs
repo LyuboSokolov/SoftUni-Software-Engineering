72869eb2ea655cc3e067b56222342d3be8a90cbc
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-24 22:47:49 +0200

    add exercise

---
+++ C#Fundamentals/18.TextProcessingExercise/MultiplyBigNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace MultiplyBigNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstNumber = Console.ReadLine().TrimStart('0');
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            StringBuilder results = new StringBuilder();
+            int temp = 0;
+
+            if (secondNumber == 0 || firstNumber == string.Empty)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            foreach (char ch in firstNumber.Reverse())
+            {
+                int digit = int.Parse(ch.ToString());
+                int result = digit * secondNumber + temp;
+
+                int restDigit = result % 10;
+                temp = result / 10;
+
+                results.Insert(0, restDigit);
+            }
+
+            if (temp > 0)
+            {
+                results.Insert(0, temp);
+            }
+            Console.WriteLine(results.ToString());
+        }
+    }
+}
