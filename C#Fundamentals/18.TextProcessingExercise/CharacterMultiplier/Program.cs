b3b2d60fc83470d0c3309639432f732495c60ca8
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-24 22:48:36 +0200

    add exercise

---
+++ C#Fundamentals/18.TextProcessingExercise/CharacterMultiplier/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CharacterMultiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            double sum = CharacterSum(input[0], input[1]);
+
+            Console.WriteLine(sum);
+        }
+
+        private static double CharacterSum(string word1, string word2)
+        {
+
+            double multiplied = 0;
+            for (int i = 0; i < Math.Max(word1.Length, word2.Length); i++)
+            {
+                double currentMultiplied = 0;
+
+                if (word1.Length > i && word2.Length > i)
+                {
+                    currentMultiplied = word1[i] * word2[i];
+                }
+                else if (word1.Length > i)
+                {
+                    currentMultiplied = word1[i];
+                }
+                else if (word2.Length > i)
+                {
+                    currentMultiplied = word2[i];
+                }
+
+
+                multiplied += currentMultiplied;
+            }
+            return multiplied;
+        }
+    }
+}
