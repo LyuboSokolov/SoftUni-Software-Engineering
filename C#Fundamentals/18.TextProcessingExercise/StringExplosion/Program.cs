5a14cdfbf1a2dca9ef989c20c3d5010e9c7679a4
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-25 21:25:08 +0200

    add exercise

---
+++ C#Fundamentals/18.TextProcessingExercise/StringExplosion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace StringExplosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string result = string.Empty;
+
+            int addStrength = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (input[i] == '>')
+                {
+                    if ((input[i + 1] >= 48 && input[i + 1] <= 57) == false)
+                    {
+                        continue;
+                    }
+                    int strength = (int.Parse(input[i + 1].ToString()));
+                    strength += addStrength;
+                    addStrength = 0;
+
+                    for (int j = i + 1; j < i + 1 + strength; j++)
+                    {
+                        if (input[j] == '>')
+                        {
+                            addStrength++;
+
+                            continue;
+                        }
+                        input = input.Remove(j, 1);
+                        j--;
+                        strength--;
+                    }
+                }
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
---
+++ C#Fundamentals/18.TextProcessingExercise/ValidUsernames/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine()
+                 .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                 .ToList();
+
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                string currentWord = input[i];
+
+                for (int j = 0; j < currentWord.Length; j++)
+                {
+                    char symbol = currentWord[j];
+
+                    if (((symbol >= 47 && symbol <= 57) ||
+                        (symbol >= 65 && symbol <= 90) ||
+                        (symbol >= 97 && symbol <= 122) ||
+                        symbol == 95 ||
+                        symbol == 45) == false)
+                    {
+                        input.RemoveAt(i);
+                        i--;
+                        break;
+                    }
+                    else if (currentWord.Length < 3 || currentWord.Length > 16)
+                    {
+                        input.RemoveAt(i);
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, input));
+        }
+    }
+}
