dc1265564d8cee08c7123de1524917ea00433bc8
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-27 20:03:50 +0200

    add exercise

---
+++ C#Fundamentals/20.RegularExpressionsExercise/Furniture/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @">>(?<furniture>[A-Za-z]+)<<(?<price>[0-9]+\.*[0-9]+)!(?<quantity>[0-9]+)";
+
+            Regex regex = new Regex(pattern);
+            string input = Console.ReadLine();
+            double spendMoney = 0;
+
+            Console.WriteLine("Bought furniture:");
+
+            while (input != "Purchase")
+            {
+                Match match = regex.Match(input);
+
+                if (match.Success)
+                {
+                    string name = match.Groups["furniture"].Value;
+                    double price = double.Parse(match.Groups["price"].Value);
+                    int quantity = int.Parse(match.Groups["quantity"].Value);
+                    Console.WriteLine(name);
+                    spendMoney += price * quantity;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total money spend: {spendMoney:f2}");
+        }
+    }
+}
