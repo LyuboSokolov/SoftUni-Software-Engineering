ee0fb24e4366a1bf597822b1751eb5201c7bc32a
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-02-28 20:47:18 +0200

    add exercise

---
+++ C#Fundamentals/21.MidExamPreparations/CounterStrike/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CounterStrike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int energy = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            int wins = 0;
+            bool isDefaut = false;
+
+            while (input?.ToUpper() != "END OF BATTLE")
+            {
+                int distance = int.Parse(input);
+
+                if (distance <= energy)
+                {
+                    wins++;
+                    energy -= distance;
+                }
+                else
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {wins} won battles and {energy} energy");
+                    isDefaut = true;
+                    break;
+                }
+
+                if (wins % 3 == 0)
+                {
+                    energy += wins;
+                }
+
+
+
+                input = Console.ReadLine();
+            }
+
+            if (!isDefaut)
+            {
+                Console.WriteLine($"Won battles: {wins}. Energy left: {energy}");
+            }
+        }
+    }
+}
