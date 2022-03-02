1728e9f40e461eb9a12e9da0c6640dc57db618dd
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-03-02 20:49:32 +0200

    add exercise

---
+++ C#Fundamentals/22.MidExamPreparation/HeartDelivery/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace HeartDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersOfHeartsOfEachHouse = Console.ReadLine()
+                .Split('@', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            int currentPosition = 0;
+            bool isFailed = false;
+            int count = 0;
+
+            while (command[0]?.ToUpper() != "LOVE!")
+            {
+                int index = int.Parse(command[1]);
+                currentPosition += index;
+
+
+                if (currentPosition >= numbersOfHeartsOfEachHouse.Length)
+                {
+                    currentPosition = 0;
+                }
+
+                if (numbersOfHeartsOfEachHouse[currentPosition] - 2 == 0)
+                {
+                    numbersOfHeartsOfEachHouse[currentPosition] = 0;
+                    Console.WriteLine($"Place {currentPosition} has Valentine's day.");
+                }
+                else if (numbersOfHeartsOfEachHouse[currentPosition] - 2 < 0)
+                {
+                    Console.WriteLine($"Place {currentPosition} already had Valentine's day.");
+                }
+                else
+                {
+                    numbersOfHeartsOfEachHouse[currentPosition] -= 2;
+                }
+
+                command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            Console.WriteLine($"Cupid's last position was {currentPosition}.");
+
+
+
+            for (int i = 0; i < numbersOfHeartsOfEachHouse.Length; i++)
+            {
+                if (numbersOfHeartsOfEachHouse[i] > 0)
+                {
+                    isFailed = true;
+                    count++;
+                }
+            }
+
+            if (isFailed)
+            {
+                Console.WriteLine($"Cupid has failed {count} places.");
+            }
+            else
+            {
+                Console.WriteLine($"Mission was successful.");
+            }
+        }
+    }
+}
---
+++ C#Fundamentals/22.MidExamPreparation/ShoppingList/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = Console.ReadLine()
+               .Split('!', StringSplitOptions.RemoveEmptyEntries)
+               .ToList();
+
+            string input = Console.ReadLine();
+
+
+            while (input?.ToUpper() != "GO SHOPPING!")
+            {
+                string[] tokens = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string command = tokens[0];
+                string item = tokens[1];
+
+
+                if (command == "Urgent")
+                {
+                    if (items.Contains(item))
+                    {
+                        input = Console.ReadLine();
+                        continue;
+                    }
+                    items.Insert(0, item);
+                }
+                else if (command == "Unnecessary")
+                {
+                    if (items.Contains(item))
+                    {
+                        items.Remove(item);
+                    }
+
+                }
+                else if (command == "Correct")
+                {
+                    string newItem = tokens[2];
+
+                    if (items.Contains(item))
+                    {
+                        int indexOldItem = items.IndexOf(item);
+                        items[indexOldItem] = newItem;
+                    }
+                }
+                else if (command == "Rearrange")
+                {
+                    if (items.Contains(item))
+                    {
+                        string currentItem = item;
+                        items.Remove(item);
+                        items.Add(currentItem);
+                    }
+
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ", items));
+        }
+    }
+}
