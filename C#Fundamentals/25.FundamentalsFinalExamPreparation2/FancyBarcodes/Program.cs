9a579837f822f712af2825fb2e6d65dc4da23f09
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-03-07 21:12:50 +0200

    add exercise

---
+++ C#Fundamentals/25.FundamentalsFinalExamPreparation2/FancyBarcodes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FancyBarcodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"@#+([A-Z][A-Za-z0-9]{4,}[A-Z])@#+";
+            string patterNumbers = @"\d";
+            Regex regex = new Regex(pattern);
+            Regex regexNumber = new Regex(patterNumbers);
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                Match match = regex.Match(input);
+                string barcodeGroup = "00";
+
+                if (match.Success)
+                {
+                    MatchCollection machNumber = regexNumber.Matches(input);
+                    if (machNumber.Count > 0)
+                    {
+                        barcodeGroup = string.Empty;
+                        foreach (var ch in machNumber)
+                        {
+                            barcodeGroup += ch;
+                        }
+                    }
+                    Console.WriteLine($"Product group: {barcodeGroup}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+        }
+    }
+}
