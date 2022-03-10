ebdccb5a74f350fd2b4fc8db8d866aac1d8c4b71
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-03-10 21:03:20 +0200

    add exercise

---
+++ C# Advanced/01.StacksAndQueuesLab/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> evenNumbers = new Queue<int>(input.Where(n => n % 2 == 0));
+
+            Console.WriteLine(string.Join(", ",evenNumbers));
+        }
+    }
+}
---
+++ C# Advanced/01.StacksAndQueuesLab/06.Supermarket/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Queue<string> names = new Queue<string>();
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    if (names.Count > 0)
+                    {
+                        while (names.Count > 0)
+                        {
+                            Console.WriteLine(names.Dequeue());
+                        }
+                    }
+                }
+                else
+                {
+                    names.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{names.Count} people remaining.");
+
+        }
+    }
+}
