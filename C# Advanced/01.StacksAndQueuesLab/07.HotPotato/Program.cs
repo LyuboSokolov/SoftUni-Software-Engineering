fdab4c692577a6a26203b7ecd80073904ab655d8
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-03-11 22:44:19 +0200

    add exercise

---
+++ C# Advanced/01.StacksAndQueuesLab/07.HotPotato/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+            Queue<string> kids = new Queue<string>(input);
+            int count = 0;
+
+            while (kids.Count > 1)
+            {
+                count++;
+               string kid = kids.Dequeue();
+
+                if (count == n)
+                {
+                    Console.WriteLine($"Removed {kid}");
+                    count = 0;
+                }
+                else
+                {
+                    kids.Enqueue(kid);
+                }
+                
+            }
+            Console.WriteLine($"Last is {kids.Dequeue()}");
+        }
+    }
+}
