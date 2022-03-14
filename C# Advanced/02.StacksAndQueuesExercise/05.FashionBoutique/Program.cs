f5d9af70f884b670eb6f036fb9f7e7ba40a0d2be
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-03-14 23:02:59 +0200

    add exercise

---
+++ C# Advanced/02.StacksAndQueuesExercise/05.FashionBoutique/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int capacityRack = int.Parse(Console.ReadLine());
+
+            int capacity = capacityRack;
+            Stack<int> numbers = new Stack<int>(input);
+            int countRack = 1;
+
+            while (numbers.Count > 0)
+            {
+                if (numbers.Peek() <= capacityRack)
+                {
+                    capacityRack -= numbers.Pop();
+                }
+                else
+                {
+                    countRack++;
+                    capacityRack = capacity;
+                    capacityRack -= numbers.Pop();
+                }
+            }
+            Console.WriteLine(countRack);
+        }
+    }
+}
