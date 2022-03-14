26cdaa5185f9ac638198b60c7101f3fc4fde1df4
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-03-14 21:18:34 +0200

    add exercise

---
+++ C# Advanced/02.StacksAndQueuesExercise/03.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Stack<int> numbers = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                int command = input[0];
+
+                if (command == 1)
+                {
+                    numbers.Push(input[1]);
+                }
+                else if (command == 2 && numbers.Count > 0)
+                {
+                    numbers.Pop();
+                }
+                else if (command == 3 && numbers.Count > 0)
+                {
+                    Console.WriteLine(numbers.Max());
+                }
+                else if (command == 4 && numbers.Count > 0)
+                {
+                    Console.WriteLine(numbers.Min());
+                }
+            }
+                        Console.WriteLine(string.Join(", ",numbers));
+        }
+    }
+}
