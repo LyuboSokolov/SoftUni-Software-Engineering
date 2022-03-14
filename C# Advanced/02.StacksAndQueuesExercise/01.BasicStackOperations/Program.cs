691b923fdd6e562c99b0ac81ad6dce409483fd16
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-03-14 16:39:56 +0200

    add exercise

---
+++ C# Advanced/02.StacksAndQueuesExercise/01.BasicStackOperations/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int n = input[0];
+            int popElements = input[1];
+            int number = input[2];
+
+            Stack<int> numbers = new Stack<int>();
+            int[] numberInput = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < n; i++)
+            {
+               
+                numbers.Push(numberInput[i]);
+            }
+
+            for (int i = 0; i < popElements; i++)
+            {
+                numbers.Pop();
+            }
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (numbers.Contains(number))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(numbers.Min());
+            }
+        }
+    }
+}
---
+++ C# Advanced/02.StacksAndQueuesExercise/02.BasicQueueOperations/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+              .Split()
+              .Select(int.Parse)
+              .ToArray();
+            int n = input[0];
+            int dequeueEments = input[1];
+            int number = input[2];
+
+            Queue<int> numbers = new Queue<int>();
+            int[] numberInput = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Enqueue(numberInput[i]);
+            }
+
+            for (int i = 0; i < dequeueEments; i++)
+            {
+                numbers.Dequeue();
+            }
+
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (numbers.Contains(number))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(numbers.Min());
+            }
+        }
+    }
+}
