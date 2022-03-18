60a06f645de14f8509ebd7b9687f9851e87c2c7b
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-03-18 07:16:17 +0200

    rename folder

---
+++ C# Advanced/03.MultidimesionalArraysLab/03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+            int sum = 0;
+            for (int row = 0; row < n; row++)
+            {
+                int[] inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = inputNumbers[col];
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += matrix[i, i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
