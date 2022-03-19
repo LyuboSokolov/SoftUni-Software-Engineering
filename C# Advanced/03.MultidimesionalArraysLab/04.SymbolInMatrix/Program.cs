71157db173291036f35f624711a36773d7caa52e
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-03-19 06:58:13 +0200

    add exercise

---
+++ C# Advanced/03.MultidimesionalArraysLab/04.SymbolInMatrix/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+            bool isFind = false;
+            for (int row = 0; row < n; row++)
+            {
+                string input = Console.ReadLine();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = input[col] ;
+                }
+            }
+
+            char inputSumbol = char.Parse(Console.ReadLine());
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row,col] == inputSumbol)
+                    {
+                        isFind = true;
+                        Console.WriteLine($"({row}, {col})");
+                        break;
+                    }
+                    if (isFind)
+                    {
+                        break;
+                    }
+                    
+                }
+            }
+            if (isFind == false)
+            {
+                Console.WriteLine($"{inputSumbol} does not occur in the matrix");
+            }
+        }
+    }
+}
---
+++ C# Advanced/03.MultidimesionalArraysLab/05.SquareWithMaximumSum/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.SquareWithMaximumSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = input[0];
+            int cols = input[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                input = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            int maxSum = int.MinValue;
+            int currRow = 0;
+            int currCol = 0;
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    int sum = 0;
+                    sum = matrix[row, col] + matrix[row, col + 1] + matrix[row + 1, col] + matrix[row + 1, col + 1];
+
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        currRow = row;
+                        currCol = col;
+                    }
+                }
+            }
+
+            for (int row = currRow; row < currRow + 2; row++)
+            {
+                for (int col = currCol; col < currCol + 2; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine(maxSum);
+        }
+    }
+}
