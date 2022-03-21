2df4a00574c4af86840c1bcf9bddcaa2d4d466f7
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-03-21 21:09:11 +0200

    add exercise

---
+++ C# Advanced/04.MultidimensionalArraysExercise/01. Diagonal Difference/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = inputNumbers[col];
+                }
+            }
+            int sumRight = 0;
+            int sumLeft = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                sumRight += matrix[row, row];
+                sumLeft += matrix[row, matrix.GetLength(0) - 1 - row];
+            }
+
+            Console.WriteLine(Math.Abs(sumRight-sumLeft));
+        }
+    }
+}
---
+++ C# Advanced/04.MultidimensionalArraysExercise/02.SquaresInMatrix/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int rows = input[0];
+            int cols = input[1];
+
+
+            char[,] matrix = new char[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] inputSymbols = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = inputSymbols[col];
+                }
+            }
+            int count = 0;
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    char currentSymbol = matrix[row, col];
+                    if (currentSymbol== matrix[row, col + 1] &&
+                        currentSymbol == matrix[row + 1, col] &&
+                        currentSymbol == matrix[row + 1, col + 1])
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
