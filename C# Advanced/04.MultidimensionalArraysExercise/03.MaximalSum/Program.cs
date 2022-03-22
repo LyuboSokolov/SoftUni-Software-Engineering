920d9ad9d954e7135b995414f0e7d3fb97baff1e
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-03-22 18:24:50 +0200

    add exercise

---
+++ C# Advanced/04.MultidimensionalArraysExercise/03.MaximalSum/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.MaximalSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int n = 3; // Square 3x3 -> can change this
+            int rows = input[0];
+            int cols = input[1];
+
+            int[,] matrix = new int[rows, cols];
+            int maxSum = int.MinValue;
+
+            for (int row = 0; row < rows; row++)
+            {
+                input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            int currRow = 0;
+            int currCol = 0;
+            for (int row = 0; row < rows - n + 1; row++)
+            {
+                for (int col = 0; col < cols - n + 1; col++)
+                {
+                    int currentSum = 0;
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            currentSum += matrix[row + i, col + j];
+                        }
+                    }
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                        currRow = row;
+                        currCol = col;
+                    }
+                }
+            }
+            Console.WriteLine($"Sum = {maxSum}");
+            for (int row = currRow; row < currRow + n; row++)
+            {
+                for (int col = currCol; col < currCol + n; col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Advanced/04.MultidimensionalArraysExercise/04.MatrixShuffling/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.MatrixShuffling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int rows = input[0];
+            int cols = input[1];
+
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                string[] inputData = Console.ReadLine()
+                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = inputData[col];
+                }
+            }
+
+            string[] inputCommandAndCoor = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (inputCommandAndCoor[0] != "END")
+            {
+                if (inputCommandAndCoor.Length != 5 || !inputCommandAndCoor.Contains("swap"))
+                {
+                    Console.WriteLine("Invalid input!");
+                    inputCommandAndCoor = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    continue;
+                }
+                string command = inputCommandAndCoor[0];
+                int row1 = int.Parse(inputCommandAndCoor[1]);
+                int col1 = int.Parse(inputCommandAndCoor[2]);
+                int row2 = int.Parse(inputCommandAndCoor[3]);
+                int col2 = int.Parse(inputCommandAndCoor[4]);
+
+                if (isValidCoordinates(row1, col1, row2, col2, matrix) == false)
+                {
+                    Console.WriteLine("Invalid input!");
+                    inputCommandAndCoor = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                    continue;
+                }
+                if (command == "swap")
+                {
+                    string first = matrix[row1, col1];
+                    string second = matrix[row2, col2];
+                    matrix[row1, col1] = second;
+                    matrix[row2, col2] = first;
+
+                    for (int row = 0; row < rows; row++)
+                    {
+                        for (int col = 0; col < cols; col++)
+                        {
+                            Console.Write(matrix[row, col] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                inputCommandAndCoor = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+
+        private static bool isValidCoordinates(int row1, int col1, int row2, int col2, string[,] matrix)
+        {
+            bool isValid = false;
+            if (row1 >= 0 && row1 < matrix.GetLength(0) &&
+                row2 >= 0 && row2 < matrix.GetLength(0) &&
+                col1 >= 0 && col1 < matrix.GetLength(1) &&
+                col2 >= 0 && col2 < matrix.GetLength(1))
+            {
+                isValid = true;
+            }
+            return isValid;
+        }
+    }
+}
+
