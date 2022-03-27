56cc761057b47d048af959e80ed92650af67fef0
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-03-27 11:50:38 +0300

    add exercise

---
+++ C# Advanced/06.SetsAndDictionariesExercise/01.UniqueUsernames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> names = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+            Console.WriteLine(string.Join(Environment.NewLine,names));
+        }
+    }
+}
---
+++ C# Advanced/06.SetsAndDictionariesExercise/02.SetsOfElements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nAndM = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = nAndM[0];
+            int m = nAndM[1];
+
+            HashSet<int> firstNumbers = new HashSet<int>();
+            HashSet<int> secondNumbers = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                firstNumbers.Add(int.Parse(Console.ReadLine()));
+            }
+            for (int i = 0; i < m; i++)
+            {
+                secondNumbers.Add(int.Parse(Console.ReadLine()));
+            }
+
+            firstNumbers.IntersectWith(secondNumbers);
+            Console.WriteLine(string.Join(" ",firstNumbers));
+        }
+    }
+}
