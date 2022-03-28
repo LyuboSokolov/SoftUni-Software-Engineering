6b3bfa1a9cb2ece0b49b70fcb0b14ab7c293b93d
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-03-28 20:32:39 +0300

    add exercise

---
+++ C# Advanced/06.SetsAndDictionariesExercise/03.PeriodicTable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> elements = new SortedSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputElements = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                for (int j = 0; j < inputElements.Length; j++)
+                {
+                    elements.Add(inputElements[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ",elements));
+        }
+    }
+}
