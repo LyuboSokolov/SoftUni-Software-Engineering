96f9176fcba4c18a89a998e174919d6cb083b346
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-03-31 21:54:54 +0300

    add exercise

---
+++ C# Advanced/07.FunctionalProgrammingLab/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] words = text.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+            Func<string, bool> upperCaseWord = n=> n[0] == n.ToUpper()[0];
+
+            words = words.Where(upperCaseWord).ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine,words));
+        }
+    }
+}
