fb7235d9cd451ccaa71b55423b2f30b0aa1dc6dc
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-04-01 07:16:53 +0300

    add exercise

---
+++ C# Advanced/07.FunctionalProgrammingLab/04.AddVAT/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] price = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Func<double[], double[]> func = AddVat;
+
+            double[] result = func(price);
+
+            foreach (var currPrice in result)
+            {
+                Console.WriteLine($"{currPrice:f2}");
+            }
+        }
+        static double[] AddVat(double[] price)
+        {
+            double[] result = new double[price.Length];
+
+            for (int i = 0; i < price.Length; i++)
+            {
+                result[i] = price[i]*1.20;
+            }
+            return result;
+        }
+        
+    }
+}
