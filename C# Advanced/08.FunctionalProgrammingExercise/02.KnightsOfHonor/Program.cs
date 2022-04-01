8c0195a9b3a45141df9569cb442511e6090c3b7e
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-04-01 20:15:07 +0300

    add exercise

---
+++ C# Advanced/08.FunctionalProgrammingExercise/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.KnightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] names = input.Select(x => $"Sir {x}").ToArray();
+
+            Action<string[]> printNames = x => Console.WriteLine(string.Join(Environment.NewLine, x));
+
+            printNames(names);
+        }
+    }
+}
