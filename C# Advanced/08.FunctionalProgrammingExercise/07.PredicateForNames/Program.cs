4cb263f5649667a45a51ce01dad447b8d3ab9d62
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-04-04 18:49:06 +0300

    add exercise

---
+++ C# Advanced/08.FunctionalProgrammingExercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine()
+                                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                        .ToList();
+
+            Func<List<string>, List<string>> filtredNames = name =>
+             {
+                 List<string> filtredName = new List<string>();
+                 foreach (var currentName in name)
+                 {
+                     if (currentName.Length <= n)
+                     {
+                         filtredName.Add(currentName);
+                     }
+                 }
+                 return filtredName;
+             };
+            Console.WriteLine(string.Join(Environment.NewLine, filtredNames(names)));
+        }
+    }
+}
