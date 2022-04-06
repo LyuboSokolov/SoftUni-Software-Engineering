d8ebdc9f9ec247711828639042a289d85530f41d
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-04-06 05:57:18 +0300

    add exercise

---
+++ C# Advanced/08.FunctionalProgrammingExercise/12.TriFunction/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12.TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Func<string, int, bool> func = (name, score) =>
+              {
+                  int sum = 0;
+                  foreach (var ch in name)
+                  {
+                      sum += ch;
+                  }
+                  return sum >= score;
+              };
+           
+            Console.WriteLine(string.Join(" ",names.Find(x => func(x,score))));
+        }
+    }
+}
