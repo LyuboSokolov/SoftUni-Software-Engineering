d301d689ad0a24f2fe696bd7bb6d718d0817ec77
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-04-09 14:01:06 +0300

    add exercise

---
+++ C# Advanced/10.DefiningClassesExercise/02.CreatingConstructors/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+       public static void Main(string[] args)
+        {
+            Person Pesho = new Person() { Name = "Pesho", Age = 20 };
+
+            Person Gosho = new Person();
+            Gosho.Name = "Gosho";
+            Gosho.Age = 18;
+
+            Person Stamat = new Person() { Name = "Stamat", Age = 43 };
+        }
+    }
+   
+}
+
+
