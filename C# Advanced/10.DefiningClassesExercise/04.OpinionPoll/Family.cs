512d63a1e83b433acce4811b833a1fdb8444a104
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-04-10 22:38:22 +0300

    add exercise

---
+++ C# Advanced/10.DefiningClassesExercise/04.OpinionPoll/Family.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            Familys = new List<Person>();
+        }
+
+        public List<Person> Familys { get; set; }
+
+
+
+        public void AddMember(Person member)
+        {
+            Familys.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return Familys.OrderByDescending(x => x.Age).First();
+        }
+
+        public List<Person> GetAllPeopleAbove()
+        {
+            return Familys.Where(p => p.Age > 30).OrderBy(p => p.Name).ToList();
+        }
+    }
+}
---
+++ C# Advanced/10.DefiningClassesExercise/04.OpinionPoll/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family family = new Family();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = input[0];
+                int age = int.Parse(input[1]);
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            List<Person> personAbove = family.GetAllPeopleAbove();
+            foreach (var person in personAbove)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+
+    }
+
+}
+
+
