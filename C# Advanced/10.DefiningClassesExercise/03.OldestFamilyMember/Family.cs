d06178d12b15f657b9eb7983850835d76c2d1d86
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-04-10 22:37:10 +0300

    add exercise

---
+++ C# Advanced/10.DefiningClassesExercise/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public Family()
+        {
+            Familys = new List<Person>();
+        }
+
+        public List<Person> Familys { get; set; }
+
+        
+
+        public void AddMember(Person member)
+        {
+            Familys.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return Familys.OrderByDescending(x => x.Age).First();
+        }
+    }
+}
