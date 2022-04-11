aec888ab56de12f99e948727ea99451808a6a4bd
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-04-11 21:17:34 +0300

    add exercise

---
+++ C# Advanced/10.DefiningClassesExercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+        public string CalculateDiferensBetweenTwoData(string startData,string endData)
+        {
+            int diference = int.Parse(((DateTime.Parse(startData) - DateTime.Parse(endData)).TotalDays).ToString());
+            diference = Math.Abs(diference);
+            
+            return diference.ToString();
+        }
+    }
+}
---
+++ C# Advanced/10.DefiningClassesExercise/05.DateModifier/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateModifier
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            string startData = Console.ReadLine();
+            string endData = Console.ReadLine();
+
+            DateModifier differenceBetweenTwoDate = new DateModifier();
+
+            string result = differenceBetweenTwoDate.CalculateDiferensBetweenTwoData(startData, endData);
+            Console.WriteLine(result);
+        }
+    }
+}
