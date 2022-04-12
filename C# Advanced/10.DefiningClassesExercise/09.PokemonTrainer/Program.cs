41b126fca1fa66030332c1aa70bd8b3988c9cb5b
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-04-12 21:49:38 +0300

    add exercise

---
+++ C# Advanced/10.DefiningClassesExercise/09.PokemonTrainer/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P09.PokemonTrainer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<Trainer> trainers = new List<Trainer>();
+
+            while (input != "Tournament")
+            {
+                string[] inputData = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string trainerName = inputData[0];
+                string pokemonName = inputData[1];
+                string pokemonElement = inputData[2];
+                int pokemonHealth = int.Parse(inputData[3]);
+
+                Pokemon currPokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+                
+                if (trainers.FirstOrDefault(x => x.Name == trainerName) == null)
+                {
+                    Trainer currTrainer = new Trainer(trainerName, currPokemon);
+                    trainers.Add(currTrainer);
+                }
+                else
+                {
+                    trainers.First(x => x.Name == trainerName).Pokemons.Add(currPokemon);
+                }
+               
+                input = Console.ReadLine();
+            }
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                if (command == "Fire")
+                {
+                    foreach (var trainer in trainers)
+                    {
+                        if (trainer.IsContainCorrectPokemon(command))
+                        {
+                            trainer.Badges++;
+                        }
+                        else
+                        {
+                            trainer.PokemonLoseHealt();
+                        }
+                    }
+                }
+                else if (command == "Water")
+                {
+                    foreach (var trainer in trainers)
+                    {
+                        if (trainer.IsContainCorrectPokemon(command))
+                        {
+                            trainer.Badges++;
+                        }
+                        else
+                        {
+                            trainer.PokemonLoseHealt();
+                        }
+                    }
+                }
+                else if (command == "Electricity")
+                {
+                    foreach (var trainer in trainers)
+                    {
+                        if (trainer.IsContainCorrectPokemon(command))
+                        {
+                            trainer.Badges++;
+                        }
+                        else
+                        {
+                            trainer.PokemonLoseHealt();
+                        }
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+            foreach (var trainer in trainers.OrderByDescending(x=> x.Badges))
+            {
+                Console.WriteLine($"{trainer.Name} {trainer.Badges} {trainer.Pokemons.Count}");
+            }
+        }
+    }
+}
---
+++ C# Advanced/10.DefiningClassesExercise/09.PokemonTrainer/Trainer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P09.PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string name, Pokemon pokemon)
+        {
+            Name = name;
+            Pokemons = new List<Pokemon>();
+            Pokemons.Add(pokemon);
+
+        }
+        public string Name { get; set; }
+
+        public int Badges { get; set; }
+
+        public List<Pokemon> Pokemons { get; set; }
+
+        public bool IsContainCorrectPokemon(string command)
+        {
+            foreach (var currPokemon in Pokemons)
+            {
+                if (currPokemon.Element == command)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void PokemonLoseHealt()
+        {
+            for (int i = 0; i < Pokemons.Count; i++)
+            {
+                Pokemons[i].Health -= 10;
+                if (Pokemons[i].Health <= 0)
+                {
+                    Pokemons.Remove(Pokemons[i]);
+                    i--;
+                }
+            }
+        }
+    }
+}
