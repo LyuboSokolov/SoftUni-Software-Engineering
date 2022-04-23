4b0581859b93db1891a1eac328db11eea1da20bc
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-04-23 23:32:07 +0300

    add exercise

---
+++ C#OOP/04.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+   public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (this.Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
