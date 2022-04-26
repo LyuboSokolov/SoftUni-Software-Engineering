4cb6cdd46e9e64af41342b2ab3c218bcf0dfc24c
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-04-26 23:48:11 +0300

    add exercise

---
+++ C#OOP/02.InheritanceExercise/NeedForSpeed/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CrossMotorcycle crossMotorcycle = new CrossMotorcycle(28, 25.4);
+
+            RaceMotorcycle raceMotorcycle = new RaceMotorcycle(101, 6.5);
+            ;
+        }
+    }
+}
