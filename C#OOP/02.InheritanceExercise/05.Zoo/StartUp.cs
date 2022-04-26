00ad073ba70698532f006646abf976e0f02d5570
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-04-26 23:49:34 +0300

    add exercise

---
+++ C#OOP/02.InheritanceExercise/05.Zoo/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Bear bear = new Bear("Gogi");
+            Reptile reptile = new Reptile("anti konti");
+
+            bear.Name = "choci";
+            Console.WriteLine(bear.Name);
+        }
+    }
+}
