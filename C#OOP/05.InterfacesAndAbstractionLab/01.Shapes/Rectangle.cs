fa9b7464855f7759973c1100997aa98105daf4e7
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-04-29 23:48:48 +0300

    add more exercise

---
+++ C#OOP/05.InterfacesAndAbstractionLab/01.Shapes/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width,int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+        public void Draw()
+        {
+            for (int row = 0; row < Width; row++)
+            {
+                for (int col = 0; col < Height; col++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
+
