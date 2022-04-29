94fa17889ea5dbedbef6e8734ce89706112e110c
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-04-29 23:46:49 +0300

    add exercise

---
+++ C#OOP/04.EncapsulationExerscise/06.TestTryCatch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TestTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("ab");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
