ba4c2044ecc16665f6e021ff5d744dce8aa2d2ca
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-05-02 22:46:56 +0300

    add exercise

---
+++ C#OOP/07.PolymorphismLab/03.Shapes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Circle(5.2);
+            Console.WriteLine(shape.CalculateArea());
+            Console.WriteLine(shape.CalculatePerimeter());
+            Console.WriteLine(shape.Draw());
+
+            shape = new Rectangle(4, 5);
+            Console.WriteLine(shape.CalculateArea());
+            Console.WriteLine(shape.CalculatePerimeter());
+            Console.WriteLine(shape.Draw());
+        }
+    }
+}
