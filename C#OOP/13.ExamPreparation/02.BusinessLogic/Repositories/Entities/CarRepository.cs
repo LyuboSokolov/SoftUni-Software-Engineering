93193dc77f9d28518af25e51cf1368c6f3f5f27d
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-05-13 07:15:59 +0300

    add exercise

---
+++ C#OOP/13.ExamPreparation/02.BusinessLogic/Repositories/Entities/CarRepository.cs
@@ -0,0 +1,46 @@
+﻿using EasterRaces.Models.Cars.Contracts;
+using EasterRaces.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasterRaces.Repositories.Entities
+{
+    public class CarRepository : IRepository<ICar>
+    {
+        private List<ICar> models;
+
+        public CarRepository()
+        {
+            models = new List<ICar>();
+        }
+
+        public List<ICar> Models { get => models; }
+
+        public void Add(ICar model)
+        {
+            models.Add(model);
+        }
+
+        public IReadOnlyCollection<ICar> GetAll()
+        {
+            return Models.AsReadOnly();
+        }
+
+        public ICar GetByName(string name)
+        {
+            return models.FirstOrDefault(p => p.Model == name);
+        }
+
+        public bool Remove(ICar model)
+        {
+            if (models.Contains(model))
+            {
+                models.Remove(model);
+                return true;
+            }
+            return false;
+        }
+    }
+}
