9d4ddaee6b57819f63172f2bfa7ffb616710c1b8
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-05-16 23:37:24 +0300

    add exercise

---
+++ C#OOP/15.ExamPreparation3/01.Structure/Repositories/Entities/RaceRepository.cs
@@ -0,0 +1,42 @@
+﻿using EasterRaces.Models.Races.Contracts;
+using EasterRaces.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasterRaces.Repositories.Entities
+{
+    public class RaceRepository : IRepository<IRace>
+    {
+        private List<IRace> models;
+
+        public List<IRace> Models { get => models; }
+
+        public void Add(IRace model)
+        {
+            models.Add(model);
+        }
+
+        public IReadOnlyCollection<IRace> GetAll()
+        {
+            return Models.AsReadOnly();
+        }
+
+        public IRace GetByName(string name)
+        {
+            return models.FirstOrDefault(p => p.Name == name);
+        }
+
+        public bool Remove(IRace model)
+        {
+            if (models.Contains(model))
+            {
+                models.Remove(model);
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
