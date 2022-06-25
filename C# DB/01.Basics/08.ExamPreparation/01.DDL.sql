c56100f1d638115c2f095c2b74da278a7feef05d
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-06-25 15:13:03 +0300

    add exam preparation exercise

---
+++ C# DB/01.Basics/08.ExamPreparation/01.DDL.sql
@@ -0,0 +1,68 @@
+CREATE TABLE [Clients](
+[ClientId] INT PRIMARY KEY IDENTITY,
+[FirstName] VARCHAR(50) NOT NULL,
+[LastName] VARCHAR(50) NOT NULL,
+[Phone] CHAR(12) NOT NULL
+)
+
+CREATE TABLE [Mechanics](
+[MechanicId] INT PRIMARY KEY IDENTITY,
+[FirstName] VARCHAR(50) NOT NULL,
+[LastName] VARCHAR(50) NOT NULL,
+[Address] VARCHAR(255) NOT NULL
+)
+
+CREATE TABLE [Models](
+[ModelId] INT PRIMARY KEY IDENTITY,
+[Name] VARCHAR(50) UNIQUE NOT NULL
+)
+
+CREATE TABLE [Vendors](
+[VendorId] INT PRIMARY KEY IDENTITY,
+[Name] VARCHAR(50) UNIQUE NOT NULL
+)
+
+CREATE TABLE [Parts](
+[PartId] INT PRIMARY KEY IDENTITY,
+[SerialNumber] VARCHAR(50) UNIQUE NOT NULL,
+[Description] VARCHAR(255),
+[Price] DECIMAL(6,2) NOT NULL,
+CHECK ([Price] > 0 ),
+[VendorId] INT FOREIGN KEY REFERENCES [Vendors] ([VendorId]) NOT NULL,
+[StockQty] INT DEFAULT 0 NOT NULL,
+CHECK([StockQty] >=0)
+)
+
+CREATE TABLE [Jobs](
+[JobId] INT PRIMARY KEY IDENTITY,
+[ModelId] INT FOREIGN KEY REFERENCES [Models] ([ModelId]) NOT NULL,
+[Status] VARCHAR(11) DEFAULT 'Pending' NOT NULL,
+CHECK ([Status] IN ('Pending','In Progress','Finished')),
+[ClientId] INT FOREIGN KEY REFERENCES [Clients] ([ClientId]) NOT NULL,
+[MechanicId] INT FOREIGN KEY REFERENCES [Mechanics] ([MechanicId]),
+[IssueDate] DATE NOT NULL,
+[FinishDate] DATE
+)
+
+CREATE TABLE [Orders](
+[OrderId] INT PRIMARY KEY IDENTITY,
+[JobId] INT FOREIGN KEY REFERENCES [Jobs]([JobId]) NOT NULL,
+[IssueDate] DATE NOT NULL,
+[Delivered] BIT DEFAULT 0 NOT NULL
+)
+
+CREATE TABLE [OrderParts](
+[OrderId] INT FOREIGN KEY REFERENCES [Orders]([OrderId]) NOT NULL,
+[PartId] INT FOREIGN KEY REFERENCES [Parts]([PartId]) NOT NULL,
+PRIMARY KEY([OrderId],[PartId]),
+[Quantity] INT DEFAULT 1 NOT NULL,
+CHECK([Quantity] > 0)
+)
+
+CREATE TABLE [PartsNeeded](
+[JobId] INT FOREIGN KEY REFERENCES [Jobs]([JobId]) NOT NULL,
+[PartId] INT FOREIGN KEY REFERENCES [Parts]([PartId]) NOT NULL,
+PRIMARY KEY ([JobId],[PartId]),
+[Quantity] INT DEFAULT 1 NOT NULL,
+CHECK([Quantity] > 0)
+)
