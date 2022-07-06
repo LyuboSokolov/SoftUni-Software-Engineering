12b9353380a53bbd781b6ffe906a442cc0a27a06
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-07-06 21:50:09 +0300

    add exercise

---
+++ C# DB/02.DatabasesAdvanced/03.EmployeesFullInformation/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext db = new SoftUniContext();
+            
+            Console.WriteLine(GetEmployeesFullInformation(db));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            List<Employee> employees = context.Employees.ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+            return sb.ToString();
+        }
+    }
+
+
+    
+}
