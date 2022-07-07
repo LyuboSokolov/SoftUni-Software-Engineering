50c9d358b942e793efdcd11eb883c62f12321106
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-07-07 23:38:53 +0300

    add exercise

---
+++ C# DB/02.DatabasesAdvanced/04.EmployeesWithSalaryOver50000/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext db = new SoftUniContext();
+
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(db));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            List<Employee> employees = context.Employees.ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var employee in employees.OrderBy(x => x.EmployeeId))
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName}" +
+                              $" {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+            return sb.ToString();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var employee in context.Employees.Where(x=> x.Salary > 50000).OrderBy(x=> x.FirstName).ToList())
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+            }
+
+            return sb.ToString();
+        }
+
+    }
+
+
+
+}
