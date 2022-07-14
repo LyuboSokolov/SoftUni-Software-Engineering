2f67ba754d0299bf83936ec921fb747df6a2f927
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-07-14 17:22:16 +0300

    add exercise

---
+++ C# DB/02.DatabasesAdvanced/01.EntityFrameworkIntroduction/09.Employee147/StartUp.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext db = new SoftUniContext();
+
+            Console.WriteLine(GetEmployee147(db));
+        }
+
+        // Problem 7 - Employees and Projects
+        public static string GetEmployeesInPeriod(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+           .Where(p => p.EmployeesProjects
+           .Any(d => d.Project.StartDate.Year >= 2001 && d.Project.StartDate.Year <= 2003))
+           .Select(
+            e => new
+            {
+                e.FirstName,
+                e.LastName,
+                Manager = e.Manager,
+                EmployeesProjects = e.EmployeesProjects
+                .Select(p => new
+                {
+                    ProjectName = p.Project.Name,
+                    StartDate = p.Project.StartDate,
+                    EndDate = p.Project.EndDate
+                })
+
+            }
+            ).ToArray();
+
+            foreach (var e in employees.Take(10))
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} - Manager: {e.Manager.FirstName} {e.Manager.LastName}");
+
+                foreach (var project in e.EmployeesProjects)
+                {
+                    sb.AppendLine($"--{project.ProjectName} -" +
+                                       $" {project.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture)} - " +
+                                       $"{(project.EndDate == null ? "not finished" : project.EndDate.Value.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture))}");
+                }
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        // Problem 8 - Addresses by Town
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var addresses = context.Addresses
+                                   .Select(x => new
+                                   {
+                                       EmployeesCount = x.Employees.Count,
+                                       x.AddressText,
+                                       TownName = x.Town.Name
+                                   })
+                                   .OrderByDescending(x => x.EmployeesCount)
+                                   .ThenBy(x => x.TownName)
+                                   .ThenBy(x => x.AddressText)
+                                   .Take(10)
+                                   .ToList();
+
+            foreach (var address in addresses)
+            {
+                sb.AppendLine($"{address.AddressText}, {address.TownName} - {address.EmployeesCount} employees");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+
+        public static string GetEmployee147(SoftUniContext context)
+        {
+            var employee147 = context.Employees
+                .Where(x => x.EmployeeId == 147)
+
+                .Select(x => new
+                {
+                    x.FirstName,
+                    x.LastName,
+                    x.JobTitle,
+                    projects = x.EmployeesProjects.Select(p => p.Project)
+                })
+                .First();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{employee147.FirstName} {employee147.LastName} - {employee147.JobTitle}");
+
+            foreach (var project in employee147.projects.OrderBy(x=> x.Name))
+            {
+                sb.AppendLine($"{ project.Name}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+
+
+
+}
