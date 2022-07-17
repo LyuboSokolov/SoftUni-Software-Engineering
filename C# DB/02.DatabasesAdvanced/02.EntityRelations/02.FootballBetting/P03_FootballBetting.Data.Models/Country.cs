924bd97b16bfa996a2dc883bdc768711c9112743
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-07-17 21:52:00 +0300

    add exercise

---
+++ C# DB/02.DatabasesAdvanced/02.EntityRelations/02.FootballBetting/P03_FootballBetting.Data.Models/Country.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+    public class Country
+    {
+        public Country()
+        {
+            Towns = new HashSet<Town>();
+        }
+        [Key]
+        public int CountryId { get; set; }
+
+        [StringLength(75)]
+        [Required]
+        public string Name { get; set; }
+
+        public  virtual ICollection<Town> Towns { get; set; }
+    }
+}
---
+++ C# DB/02.DatabasesAdvanced/02.EntityRelations/02.FootballBetting/P03_FootballBetting.Data.Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+   public  class User
+    {
+        public User()
+        {
+            Bets = new HashSet<Bet>(); 
+        }
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(75)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public int Password { get; set; }
+
+        [Required]
+        [StringLength(75)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public virtual ICollection<Bet> Bets { get; set; }
+    }
+}
