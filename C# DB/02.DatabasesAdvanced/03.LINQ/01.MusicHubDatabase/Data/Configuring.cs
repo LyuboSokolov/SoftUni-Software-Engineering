dc5130685c195136799b2c22f640f53cb16f0515
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-07-17 21:54:36 +0300

    add exercise

---
+++ C# DB/02.DatabasesAdvanced/03.LINQ/01.MusicHubDatabase/Data/Configuring.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicHub.Data
+{
+    public static class Configuring
+    {
+        public const string ConnectionString = @"Server = DESKTOP-SRF0PGM\SQLEXPRESS;
+                                                 DataBase = MusicHub;Integrated Security = True;";
+    }
+}
---
+++ C# DB/02.DatabasesAdvanced/03.LINQ/01.MusicHubDatabase/Data/Models/Enumeration/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicHub.Data.Models.Enumeration
+{
+    public enum Genre
+    {
+        Blues = 1,
+        Rap = 2,
+        PopMusic = 3,
+        Rock = 4,
+        Jazz = 5
+    }
+}
