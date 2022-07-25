0fa7c29a13cf7e503a653963823223bd0c9bd60c
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-07-25 23:12:27 +0300

    add exerise

---
+++ C# DB/02.DatabasesAdvanced/05.JavaScriptObjectNotation_JSON/01.ImportUsers/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Dtos.Input;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var dbcontext = new ProductShopContext();
+
+            //dbcontext.Database.EnsureDeleted();
+            //dbcontext.Database.EnsureCreated();
+
+
+            var usersJsonAsString = File.ReadAllText("../../../Datasets/users.json");
+
+            Console.WriteLine(ImportUsers(dbcontext, usersJsonAsString));
+
+
+        }
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            IEnumerable<UserInputDto> users = JsonConvert.DeserializeObject<IEnumerable<UserInputDto>>(inputJson);
+
+            var mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            });
+
+            IMapper mapper = new Mapper(mapperConfiguration);
+
+            var mappedUsers = mapper.Map<IEnumerable<User>>(users);
+
+            context.Users.AddRange(mappedUsers);
+            context.SaveChanges();
+
+            return $"Successfully imported {mappedUsers.Count()}";
+        }
+    }
+}
