9e850945b65e047c6c8c1ca7119054fc6d38f630
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-07-25 23:13:30 +0300

    add exericse

---
+++ C# DB/02.DatabasesAdvanced/05.JavaScriptObjectNotation_JSON/02.ImportProducts/StartUp.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Dtos.Input;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var dbcontext = new ProductShopContext();
+
+            //dbcontext.Database.EnsureDeleted();
+            //dbcontext.Database.EnsureCreated();
+
+            //var usersJsonAsString = File.ReadAllText("../../../Datasets/users.json");
+
+            var producJsonAsString = File.ReadAllText("../../../Datasets/products.json");
+            
+            Console.WriteLine(ImportProducts(dbcontext, producJsonAsString));
+
+
+        }
+
+        //Problem Query 2. Import Users
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            IEnumerable<UserInputDto> users = JsonConvert.DeserializeObject<IEnumerable<UserInputDto>>(inputJson);
+
+            var mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            });
+
+            IMapper mapper = new Mapper(mapperConfiguration);
+
+            var mappedUsers = mapper.Map<IEnumerable<User>>(users);
+
+            context.Users.AddRange(mappedUsers);
+            context.SaveChanges();
+
+            return $"Successfully imported {mappedUsers.Count()}";
+        }
+
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            IEnumerable<ProductInputDto> products = JsonConvert
+                                                    .DeserializeObject<IEnumerable<ProductInputDto>>(inputJson);
+
+            var mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            });
+            IMapper mapper = new Mapper(mapperConfiguration);
+
+            var mappedProducts = mapper.Map<IEnumerable<Product>>(products);
+
+            context.Products.AddRange(mappedProducts);
+            context.SaveChanges();
+
+            return $"Successfully imported {mappedProducts.Count()}";
+        }
+    }
+}
