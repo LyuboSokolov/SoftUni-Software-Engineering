d26084128f3203ed8d3bc999a973fd18360468d8
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-08-08 23:46:58 +0300

    Add exercise

---
+++ C# DB/02.DatabasesAdvanced/08.ExamPreparation2/01.ModelDefition/Data/Models/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TeisterMask.Data.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(40,MinimumLength =3)]
+        [Required]
+        [RegularExpression(@"\w||\d")]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        public ICollection<EmployeeTask> EmployeesTasks { get; set; }
+
+        public Employee()
+        {
+            EmployeesTasks = new List<EmployeeTask>();
+        }
+
+        // •Id - integer, Primary Key
+        //•	Username - text with length[3, 40]. Should contain only lower or upper case letters and/or digits. (required)
+        //•	Email – text(required). Validate it! There is attribute for this job.
+        //•	Phone - text.Consists only of three groups(separated by '-'), the first two consist of three digits and the last one - of 4 digits. (required)
+        //•	EmployeesTasks - collection of type EmployeeTask
+
+    }
+}
---
+++ C# DB/02.DatabasesAdvanced/08.ExamPreparation2/01.ModelDefition/Data/Models/Project.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TeisterMask.Data.Models
+{
+    public class Project
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(40,MinimumLength =2)]
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime OpenDate { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public ICollection<Task> Tasks { get; set; }
+
+        public Project()
+        {
+            Tasks = new List<Task>();
+        }
+        //•	Id - integer, Primary Key
+        //•	Name - text with length[2, 40] (required)
+        //•	OpenDate - date and time(required)
+        //•	DueDate - date and time(can be null)
+        //•	Tasks - collection of type Task
+
+    }
+}
