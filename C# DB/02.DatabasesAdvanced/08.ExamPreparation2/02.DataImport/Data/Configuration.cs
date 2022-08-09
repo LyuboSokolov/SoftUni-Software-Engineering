43a3b28585145a572dbaa3700e0d67ec42adff09
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-08-09 23:26:51 +0300

    Add exercise

---
+++ C# DB/02.DatabasesAdvanced/08.ExamPreparation2/02.DataImport/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace TeisterMask.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-SRF0PGM\SQLEXPRESS;Database=TeisterMask;Trusted_Connection=True";
+    }
+}
---
+++ C# DB/02.DatabasesAdvanced/08.ExamPreparation2/02.DataImport/DataProcessor/ImportDto/ImportTaskDto.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+using TeisterMask.Data.Models.Enums;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    [XmlType("Task")]
+    public class ImportTaskDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [StringLength(40,MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [XmlElement("OpenDate")]
+        [Required]
+        public string OpenDate { get; set; }
+
+        [XmlElement("DueDate")]
+        [Required]
+        public string DueDate { get; set; }
+
+        [XmlElement("ExecutionType")]
+        [Required]
+        public string ExecutionType { get; set; }
+
+        [XmlElement("LabelType")]
+        [Required]
+        public string LabelType { get; set; }
+
+
+       //•	Name - text with length[2, 40] (required)
+        //•	OpenDate - date and time(required)
+        //•	DueDate - date and time(required)
+        //•	ExecutionType - enumeration of type ExecutionType, with possible values(ProductBacklog, SprintBacklog, InProgress, Finished) (required)
+        //•	LabelType - enumeration of type LabelType, with possible values(Priority, CSharpAdvanced, JavaAdvanced, EntityFramework, Hibernate) (required)
+        
+
+    }
+}
