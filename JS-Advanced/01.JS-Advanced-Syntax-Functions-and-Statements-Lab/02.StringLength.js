bc47dbda88e6ad146f118a1d92411adfcae8aa2b
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-10-05 07:21:00 +0300

    add exercise

---
+++ JS-Advanced/01.JS-Advanced-Syntax-Functions-and-Statements-Lab/02.StringLength.js
@@ -0,0 +1,5 @@
+function solve(firstWord,secondWord,thirdWord) {  
+let sum = firstWord.length + secondWord.length + thirdWord.length;
+console.log(sum);
+console.log(Math.floor(sum/3));
+};
---
+++ JS-Advanced/01.JS-Advanced-Syntax-Functions-and-Statements-Lab/03.LargestNumber.js
@@ -0,0 +1,11 @@
+function solve(firstNumber,secondNumber,thirdNumber){
+    let result;
+    if(firstNumber > secondNumber && firstNumber > thirdNumber){
+        result = firstNumber;
+    } else if (secondNumber > firstNumber && secondNumber > thirdNumber){
+        result = secondNumber;
+    } else{
+        result = thirdNumber;
+    }
+    console.log(`The largest number is ${result}.`);
+}
