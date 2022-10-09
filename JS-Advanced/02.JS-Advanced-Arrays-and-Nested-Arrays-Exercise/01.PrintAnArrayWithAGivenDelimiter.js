92183f94a96541a2b5800a20a8c3c5b52de10117
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-10-09 09:50:53 +0300

    Add exercise

---
+++ JS-Advanced/02.JS-Advanced-Arrays-and-Nested-Arrays-Exercise/01.PrintAnArrayWithAGivenDelimiter.js
@@ -0,0 +1,4 @@
+function solve(input, delimiter) {
+    console.log(input.join(delimiter));
+}
+solve(['One', 'Two', 'Three', 'Four', 'Five'], '-');
---
+++ JS-Advanced/02.JS-Advanced-Arrays-and-Nested-Arrays-Exercise/02.PrintEveryN-thElementFromAnArray.js
@@ -0,0 +1,8 @@
+function solve(input, step) {
+    let result = [];
+    for (let i = 0; i < input.length; i += step) {
+        result.push(input[i]);
+    }
+    return result;
+}
+solve(['5', '20', '31', '4', '20'], 2);
