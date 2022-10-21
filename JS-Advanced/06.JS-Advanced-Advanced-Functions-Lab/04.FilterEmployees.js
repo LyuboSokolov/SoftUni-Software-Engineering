40d621626b70282d1dd0b862c88f54f6a9aca498
LyuboSokolov <lyubomir.sokolov95@gmail.com>
2022-10-21 22:20:45 +0300

    add exercise

---
+++ JS-Advanced/06.JS-Advanced-Advanced-Functions-Lab/04.FilterEmployees.js
@@ -0,0 +1,32 @@
+function solve(input, filter) {
+    let [criteria, value] = filter.split('-');
+
+    let data = (JSON.parse(input))
+        .filter(x => x[criteria] == value)
+        .map((x, i) => {
+            console.log(`${i}. ${x.first_name} ${x.last_name} - ${x.email}`)
+        });
+}
+
+solve(`[{
+    "id": "1",
+    "first_name": "Ardine",
+    "last_name": "Bassam",
+    "email": "abassam0@cnn.com",
+    "gender": "Female"
+  }, {
+    "id": "2",
+    "first_name": "Kizzee",
+    "last_name": "Jost",
+    "email": "kjost1@forbes.com",
+    "gender": "Female"
+  },  
+{
+    "id": "3",
+    "first_name": "Evanne",
+    "last_name": "Maldin",
+    "email": "emaldin2@hostgator.com",
+    "gender": "Male"
+  }]`,
+    'gender-Female'
+);
